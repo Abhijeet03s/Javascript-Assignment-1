f717280eb14ee44e487e61cb0f1b74a6c735790c
Abhijeet03s <abhijeetsharma476@gmail.com>
2022-08-30 11:50:15 +0530

    solved

--- 34-linear.js
+++ 34-linear.js
@@ -1,14 +1,12 @@
 // 34. Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
 
-let x;
-let y;
+let x,y;
 
 function solveLinEquation(a, b, c) {
-  let eq = a * x + b * y + c;
+  let eq = ((a * x) + (b * y)) + c;
   y = -c / b;
   x = -c / a;
   console.log(`${x}`);
   console.log(`${y}`);
 }
-
 solveLinEquation(10, 20, -30);
