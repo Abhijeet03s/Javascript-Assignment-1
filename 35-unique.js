ba564319970f0b6b9687ae1e08a650fa1ef36c88
Abhijeet03s <abhijeetsharma476@gmail.com>
2022-09-15 19:14:12 +0530

    initial commit

--- 35-unique.js
+++ 35-unique.js
@@ -1,1 +1,27 @@
 // 35. Write a functions which checks if all items are unique in the array.
+
+
+let arr = ['car', 10, 'scooter', 100, 'train', -5, 50, 'jet', 200, 'bicycle'];
+
+let count = 0;
+
+let prev;
+
+for (let i = 0; i < arr.length && count <= 1; i++) {
+    count = 0;
+    prev = arr[i];
+    arr.forEach(e => {
+        if (e === prev) {
+            count++;
+        }
+    });
+}
+
+// console.log(count);
+
+if (count == 1) {
+    console.log(`All items are unique`);
+}
+else {
+    console.log(`All items are not unique`);
+}
